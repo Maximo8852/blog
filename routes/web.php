310b9d40c9f093e6de808f6b2bb6246c6fabd68a
Maximo8852 <33952454+Maximo8852@users.noreply.github.com>
2018-03-01 17:26:17 +0600

    post action

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', 'PostsController@index');
+
+Route::get('posts/create', 'PostsController@create');
+
+Route::get('posts/{post}', 'PostsController@show');
+
+Route::post('/post', 'PostsController@store');
+
+Route::get('/posts/{post}/edit', 'PostsController@edit');
+
+Route::patch('posts/{post}', 'PostsController@update');
+
+Route::delete('posts/{post}', 'PostsController@destroy');
